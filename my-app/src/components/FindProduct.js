32edf126d180378aa78a2bad73e24c3a10f7da48
w280749941 <wshaoxiang10@yahoo.com>
2017-07-16 22:44:51 -0400

    Fixed search bar with { restUrl }

--- my-app/src/components/FindProduct.js
+++ my-app/src/components/FindProduct.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Thumbnail from './Thumbnail';
-import restUrl from '../index';
+import { restUrl } from '../index';
 
 class FindProduct extends Component {
   constructor({match}) {
